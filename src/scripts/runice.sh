3a02c5770325a8b99b7996a61a5d6b7fd848e2bd
Yifan He <yhe@cs.nyu.edu>
2015-05-06 23:49:35 -0400

    trying to support bin release script

--- src/scripts/runice.sh
+++ src/scripts/runice.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-ICE_HOME=~/code/ICE/target
-ICE_LIB_HOME=~/code/ICE/lib
-java -Xmx4g -cp "$ICE_HOME/ICE-0.2.0-jar-with-dependencies.jar:$ICE_LIB_HOME/Jet-1.8.0.11-ICE-jar-with-dependencies.jar" edu.nyu.jet.ice.controllers.Nice
+ICE_HOME=.
+ICE_LIB_HOME=.
+java -Xmx4g -cp "$ICE_HOME/ICE-0.2.0-jar-with-dependencies.jar:$ICE_LIB_HOME/jet-all.jar" edu.nyu.jet.ice.controllers.Nice
