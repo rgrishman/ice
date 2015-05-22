34a7769767ef198d8b8220a3f79294e5fe8485ca
Yifan He <yhe@cs.nyu.edu>
2015-05-22 01:17:53 -0400

    switching entity panel

--- src/main/java/edu/nyu/jet/ice/views/swing/SwingEntitiesPanel.java
+++ src/main/java/edu/nyu/jet/ice/views/swing/SwingEntitiesPanel.java
@@ -5,32 +5,31 @@ import edu.nyu.jet.ice.views.Refreshable;
 import net.miginfocom.swing.MigLayout;
 
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * Created by yhe on 10/13/14.
  */
 public class SwingEntitiesPanel extends JPanel implements Refreshable {
+    private SwingIceStatusPanel statusPanel = new SwingIceStatusPanel();
+
     public SwingEntitiesPanel() {
         super();
         this.setLayout(new MigLayout());
         this.setOpaque(false);
+        this.removeAll();
+        JPanel termBox = new JPanel();
+        termBox.setOpaque(true);
+        termBox.setMinimumSize(new Dimension(480, 270));
+        Box indexBox = Ice.selectedCorpus.entitySetBuilder.makeSwingBox();
+        this.add(termBox, "cell 0 0");
+        this.add(statusPanel, "cell 1 0 1 2");
+        this.add(indexBox, "cell 0 1");
         refresh();
-//        Box termBox = Ice.selectedCorpus.swingTermBox();
-//        Box indexBox = Ice.selectedCorpus.entitySetBuilder.makeSwingBox();
-//        this.add(termBox, "cell 0 0");
-//        SwingIceStatusPanel statusPanel = new SwingIceStatusPanel();
-//        this.add(statusPanel, "cell 1 0 1 2");
-//        this.add(indexBox, "cell 0 1");
     }
 
     public void refresh() {
-        this.removeAll();
-        Box termBox = Ice.selectedCorpus.swingTermBox();
-        Box indexBox = Ice.selectedCorpus.entitySetBuilder.makeSwingBox();
-        this.add(termBox, "cell 0 0");
-        SwingIceStatusPanel statusPanel = new SwingIceStatusPanel();
-        this.add(statusPanel, "cell 1 0 1 2");
-        this.add(indexBox, "cell 0 1");
+
         statusPanel.refresh();
     }
 }
