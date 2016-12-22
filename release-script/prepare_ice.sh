7afc5e0dd6226a44e7a9a22bc4de3809749b33c6
Ralph Grishman <grishman@proteus100.cs.nyu.edu>
2016-12-21 23:26:03 -0500

    Build release from branch newmaster (instead of workspace).

--- release-script/prepare_ice.sh
+++ release-script/prepare_ice.sh
@@ -1,37 +1,40 @@
-#!/usr/bin/env bash
+#!/usr/bin/tcsh
 #
 #  input: JET distribution tar
-#         $ICE_HOME
+#         clone-dir
 #  output:  ICE distribution tar
 #
 # Run this script in a fresh directory
 #
-JET_PACKAGE=$JET_HOME/export/jet-161201.tar.gz
-export JAVA_TOOL_OPTIONS="-Dfile.encoding=UTF-8"
+setenv JET_PACKAGE $JET_HOME/export/jet-161201.tar.gz
+export JAVA_TOOL_OPTIONS "-Dfile.encoding=UTF-8"
 curl http://daringfireball.net/projects/downloads/Markdown_1.0.1.zip > Markdown_1.0.1.zip
 unzip Markdown_1.0.1.zip
 echo "Unpacking Jet package..."
 tar zxvf $JET_PACKAGE
 mv props jet-props
-cp jet-all.jar $ICE_HOME/lib
-pushd $ICE_HOME
-# ant dist-all-jar
+git clone https://github.com/rgrishman/ice.git clone-dir
+cp jet-all.jar clone-dir/lib
+pushd clone-dir
+git checkout newmaster
+ant dist-all-jar
 popd
-perl Markdown_1.0.1/Markdown.pl $ICE_HOME/README.md > README.html
-perl Markdown_1.0.1/Markdown.pl $ICE_HOME/docs/iceman.md > docs/iceman.html
-perl Markdown_1.0.1/Markdown.pl $ICE_HOME/docs/ICE_Design.md > docs/ICE_Design.html
-cp $ICE_HOME/docs/*.png docs/
-cp $ICE_HOME/LICENSE ./
-cp $ICE_HOME/COPYRIGHT ./
+cp clone-dir/ice-all.jar .
+perl Markdown_1.0.1/Markdown.pl clone-dir/README.md > README.html
+perl Markdown_1.0.1/Markdown.pl clone-dir/docs/iceman.md > docs/iceman.html
+perl Markdown_1.0.1/Markdown.pl clone-dir/docs/ICE_Design.md > docs/ICE_Design.html
+cp clone-dir/docs/*.png docs/
+cp clone-dir/LICENSE ./
+cp clone-dir/COPYRIGHT ./
 #  scripts
-cp $ICE_HOME/src/scripts/runice.sh ./bin
-cp $ICE_HOME/src/scripts/runtagger.sh ./bin
-cp $ICE_HOME/src/scripts/icecli ./bin
-cp $ICE_HOME/src/scripts/icecli6 ./bin
+cp clone-dir/src/scripts/runice.sh ./bin
+cp clone-dir/src/scripts/runtagger.sh ./bin
+cp clone-dir/src/scripts/icecli ./bin
+cp clone-dir/src/scripts/icecli6 ./bin
 #  ice.yml, iceprops, onomaprops, parseprops, props
-cp $ICE_HOME/src/props/* ./
+cp clone-dir/src/props/* ./
 #  quantifierPatterns and ACE DTD
-cp $ICE_HOME/src/models/data/* ./data/
+cp clone-dir/src/models/data/* ./data/
 #  files for export from ICE
 touch acedata/ice_onoma.dict
 touch acedata/EDTypesFromUser.dict
@@ -41,6 +44,7 @@ chmod u+x bin/runtagger.sh
 chmod u+x bin/icecli6
 chmod u+x bin/icecli
 rm -rf Markdown*
-eco "Building ICE tar"
+rm -rf clone-dir
+echo "Building ICE tar"
 set date = `date +'%y%m%d'`
 tar zcvf ice-$date.tar.gz *
