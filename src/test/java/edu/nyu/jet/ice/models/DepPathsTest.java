4fc85cb0dd641a9b43284e0364b2d773fcea55c4
grishman <grishman@cs.nyu.edu>
2017-04-12 17:34:02 -0400

    Enlarge test for generating path and repr (phrase) for relation.

--- src/test/java/edu/nyu/jet/ice/models/DepPathsTest.java
+++ src/test/java/edu/nyu/jet/ice/models/DepPathsTest.java
@@ -4,6 +4,7 @@ import edu.nyu.jet.ice.models.DepPaths;
 import edu.nyu.jet.tipster.*;
 import edu.nyu.jet.lisp.FeatureSet;
 import edu.nyu.jet.parser.SyntacticRelationSet;
+import edu.nyu.jet.parser.DepTransformer;
 
 import java.util.*;
 import java.io.*;
@@ -14,14 +15,27 @@ import org.junit.Test;
 import org.junit.BeforeClass;
 
 /**
- *  basic tests for DepPaths.buildSyntacticPathOnSpans
+ *  basic tests for DepPaths.buildSyntacticPathOnSpans and linearize:
+ *        minimal SVO;
+ *        perfect and PP;
+ *        passive.
  */
 
 public class DepPathsTest {
 
+    private static DepTransformer transformer;
+    private static DepPathRegularizer depPathRegularizer;
+
+    @BeforeClass
+    public static void depPathsTest () {
+	transformer = new DepTransformer("yes");
+	transformer.setUsePrepositionTransformation(false);
+	depPathRegularizer = new DepPathRegularizer();
+    }
+
     @Test
     public void buildSyntacticPathTest1 () throws IOException {
-	// Fred_Smith visited Chicago.
+	Document doc = new Document("Fred Smith visited Chicago.");
 	Span arg1 = new Span(0, 11);
 	Span arg2 = new Span(19, 27);
 	int from = 0;
@@ -30,19 +44,26 @@ public class DepPathsTest {
 	relations.read(new BufferedReader (new StringReader(
 	"nsubj | visited | 11 | VBD | Fred_Smith | 0 | NNP\n" +
 	"dobj | visited | 11 | VBD | Chicago | 19 | NNP\n" +
-	"punct | visited | 11 | VBD | . | 27 | .\n" +
-	"nsubj-1 | Fred_Smith | 0 | ? | visited | 11 | ?\n" +
-	"dobj-1 | Chicago | 19 | ? | visited | 11 | ?\n" +
-	"punct-1 | . | 27 | ? | visited | 11 | ?")));
+	"punct | visited | 11 | VBD | . | 27 | .\n"))); 
+	SyntacticRelationSet transformedRelations = 
+	    transformer.transform(relations.deepCopy(), doc.fullSpan());
+	relations = transformedRelations;
+	relations.addInverses();
 	List<Span> localSpans = new ArrayList<Span>();
 
-	DepPath p = DepPaths.buildSyntacticPathOnSpans (from, to, arg1, arg2, relations,localSpans);
-	assertEquals(p.toString(), "nsubj-1:visit:dobj");
+	DepPath path = DepPaths.buildSyntacticPathOnSpans (from, to, arg1, arg2, relations,localSpans);
+	assertEquals(path.toString(), "nsubj-1:visit:dobj");
+
+	DepPath regularizedPath = depPathRegularizer.regularize(path);
+	String type1 = "PERSON";
+	String type2 = "GPE";
+	String linearizedPath = regularizedPath.linearize(doc, relations, type1, type2);
+	System.out.println(" linearized pah:  " + linearizedPath);
     }
 
     @Test
     public void buildSyntacticPathTest2 () throws IOException {
-	// Fred_Smith has lived in Chicago.
+	Document doc = new Document("Fred Smith has lived in Chicago.");
 	// test:  transformation, prep
 	Span arg1 = new Span(0, 11);
 	Span arg2 = new Span(24, 32);
@@ -50,18 +71,62 @@ public class DepPathsTest {
 	int to = 24;
 	SyntacticRelationSet relations = new SyntacticRelationSet();
 	relations.read(new BufferedReader (new StringReader(
-	"nsubj | lived | 15 | VBZ | Fred_Smith | 0 | NNP\n" +
+	// "nsubj | lived | 15 | VBZ | Fred_Smith | 0 | NNP\n" +
+	"nsubj | has | 11 | VBZ | Fred_Smith | 0 | NNP\n" +
+	"vch | has | 11 | VBZ | lived | 15 | VBN\n" +
 	"prep | lived | 15 | VBN | in | 21 | IN\n" +
 	"pobj | in | 21 | IN | Chicago | 24 | NNP\n" +
-	"punct | lived | 15 | VBZ | . | 32 | .\n" +
-	"nsubj-1 | Fred_Smith | 0 | ? | lived | 15 | ?\n" +
-	"prep-1 | in | 21 | ? | lived | 15 | ?\n" +
-	"pobj-1 | Chicago | 24 | ? | in | 21 | ?\n" +
-	"punct-1 | . | 32 | ? | lived | 15 | ?\n")));
+	// "punct | lived | 15 | VBZ | . | 32 | .\n")));
+	"punct | has | 11 | VBZ | . | 32 | .\n")));
+	SyntacticRelationSet transformedRelations = 
+	    transformer.transform(relations.deepCopy(), doc.fullSpan());
+	relations = transformedRelations;
+	relations.addInverses();
 	List<Span> localSpans = new ArrayList<Span>();
 
-	DepPath p = DepPaths.buildSyntacticPathOnSpans (from, to, arg1, arg2, relations,localSpans);
-	assertEquals(p.toString(), "nsubj-1:live:prep:in:pobj");
+	DepPath path = DepPaths.buildSyntacticPathOnSpans (from, to, arg1, arg2, relations,localSpans);
+	assertEquals(path.toString(), "nsubj-1:live:prep:in:pobj");
+
+	DepPathRegularizer depPathRegularizer = new DepPathRegularizer();
+	DepPath regularizedPath = depPathRegularizer.regularize(path);
+	String type1 = "PERSON";
+	String type2 = "GPE";
+	String linearizedPath = regularizedPath.linearize(doc, relations, type1, type2);
+	System.out.println(" linearized pah:  " + linearizedPath);
+    }
+
+    @Test
+    public void buildSyntacticPathTest3 () throws IOException {
+	Document doc = new Document("Fred Smith was shot in Chicago.");
+	// test:  transformation, prep
+	Span arg1 = new Span(0, 11);
+	Span arg2 = new Span(23, 30);
+	int from = 0;
+	int to = 23;
+	SyntacticRelationSet relations = new SyntacticRelationSet();
+	relations.read(new BufferedReader (new StringReader(
+	"nsubj | was | 11 | VBD | Fred_Smith | 0 | NNP\n" +
+	"vch | was | 11 | VBD | shot | 15 | VBN\n" +
+	"prep | shot | 15 | VBN | in | 20 | IN\n" +
+	"pobj | in | 20 | IN | Chicago | 23 | NNP\n" +
+	"punct | was | 11 | VBZ | . | 30 | .\n")));
+	SyntacticRelationSet tRelations = 
+	    transformer.transform(relations.deepCopy(), doc.fullSpan());
+	relations.addInverses();
+	tRelations.addInverses();
+	List<Span> localSpans = new ArrayList<Span>();
+
+	DepPath tPath = DepPaths.buildSyntacticPathOnSpans (from, to, arg1, arg2, tRelations,localSpans);
+// 	assertEquals(path.toString(), "nsubj-1:be:vch:shoot:prep:in:pobj");
+	System.out.println("tPath = " + tPath.toString());
+
+	DepPath path = DepPaths.buildSyntacticPathOnSpans (from, to, arg1, arg2, relations,localSpans);
+	DepPathRegularizer depPathRegularizer = new DepPathRegularizer();
+	DepPath regularizedPath = depPathRegularizer.regularize(path);
+	String type1 = "PERSON";
+	String type2 = "GPE";
+	String linearizedPath = regularizedPath.linearize(doc, relations, type1, type2);
+	System.out.println(" linearized pah:  " + linearizedPath);
     }
 /*  -- unit test to be completed
     @Test
