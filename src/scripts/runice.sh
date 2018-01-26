4a3c8bcc9f3d044d354240818ac000558e5e5b66
grishman <grishman@cs.nyu.edu>
2018-01-26 12:27:37 -0500

    icecli and runice.sh:  update scripts to preserve environment variables and transmit ICE_HOME to Ice.

--- src/scripts/runice.sh
+++ src/scripts/runice.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-ICE_HOME=.
-ICE_LIB_HOME=.
-java -Xmx4g -Dfile.encoding=UTF-8 -cp "$ICE_HOME/ice-all.jar" -DjetHome=$JET_HOME edu.nyu.jet.ice.controllers.Nice
+java -Xmx4g -Dfile.encoding=UTF-8 -cp "$ICE_HOME/ice-all.jar" -DjetHome=$JET_HOME -DiceHome=$ICE_HOME edu.nyu.jet.ice.controllers.Nice
