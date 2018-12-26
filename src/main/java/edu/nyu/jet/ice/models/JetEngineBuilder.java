3ef58a27fc0a20e899881129018c2c3db242bf98
Ralph Grishman <rdg@Ralphs-MacBook-Pro.local>
2018-12-26 16:50:20 -0500

    Extend JetEngineBuilder to cover events as well as relations

--- src/main/java/edu/nyu/jet/ice/models/JetEngineBuilder.java
+++ src/main/java/edu/nyu/jet/ice/models/JetEngineBuilder.java
@@ -1,21 +1,26 @@
 package edu.nyu.jet.ice.models;
 
 import edu.nyu.jet.ice.uicomps.Ice;
+import edu.nyu.jet.ice.events.*;
+
+import edu.nyu.jet.tipster.*;
 
 import java.util.*;
 import java.io.*;
 
 /**
  *  writes the following files to be read by Jet containing information on
- *  entity sets and relations.  All files are written in the
+ *  entity sets, relations, and events.  All files are written in the
  *  <code>data</code> directory of <code>JET_HOME</code>.
  *
  *  <table border="1">
- *    <tr> <th> information                </th> <th> file name        </th> </tr>
- *    <tr> <th> common nouns               </th> <th> iceEDTtype.dict  </th> </tr>
- *    <tr> <th> proper nouns               </th> <th> iceOnoma.dict    </th> </tr>
- *    <tr> <th> LDP patterns for relations </th> <th> ldpRelationModel </th> </tr>
- *    <tr> <th> negated relation patterns  </th> <th> ldpNegRelations  </th> </tr>
+ *    <tr> <th> information                </th> <th> file name         </th> </tr>
+ *    <tr> <th> common nouns               </th> <th> iceEDTtype.dict   </th> </tr>
+ *    <tr> <th> proper nouns               </th> <th> iceOnoma.dict     </th> </tr>
+ *    <tr> <th> LDP patterns for relations </th> <th> ldpRelationModel  </th> </tr>
+ *    <tr> <th> negated relation patterns  </th> <th> ldpNegRelations   </th> </tr>
+ *    <tr> <th> depTree event    patterns  </th> <th> depTreeEventModel </th> </tr>
+ *    <tr> <th> negated event    patterns  </th> <th> negatedEventModel </th> </tr>
  *  </table>
  */
 
@@ -26,7 +31,9 @@ public class JetEngineBuilder {
     private static Writer commonNounWriter = null;
     private static Writer properNounWriter = null;
     private static Writer relationPatternWriter = null;
-    private static Writer negatedPatternWriter = null;
+    private static Writer negatedRelationPatternWriter = null;
+    private static Writer eventPatternWriter = null;
+    private static Writer negatedEventPatternWriter = null;
 
     public static void setCommonNounWriter (Writer w) {
         commonNounWriter = w;}
@@ -34,8 +41,12 @@ public class JetEngineBuilder {
         properNounWriter = w;}
     public static void setRelationPatternWriter (Writer w) {
         relationPatternWriter = w;}
-    public static void setNegatedPatternWriter (Writer w) {
-        negatedPatternWriter = w;}
+    public static void setNegatedRelationPatternWriter (Writer w) {
+        negatedRelationPatternWriter = w;}
+    public static void setEventPatternWriter (Writer w) {
+        eventPatternWriter = w;}
+    public static void setNegatedEventPatternWriter (Writer w) {
+        negatedEventPatternWriter = w;}
 
     /**
      * writes files to be read by Jet containing information on
@@ -46,9 +57,9 @@ public class JetEngineBuilder {
         try {
             String jetHome = System.getProperty("jetHome");
             if (jetHome == null) {
-		System.err.println("JET_HOME not set, cannot export.");
-		return;
-	    }
+		        System.err.println("JET_HOME not set, cannot export.");
+		        return;
+	        }
             String dataDirectory = jetHome + "/data/";
             if (commonNounWriter == null) {
                 String commonNounFileName = dataDirectory + "iceEDTtype.dict";
@@ -62,19 +73,28 @@ public class JetEngineBuilder {
                 String relationPatternFileName = dataDirectory +"ldpRelationModel";
                 relationPatternWriter = new FileWriter(relationPatternFileName);
             }
-            if (negatedPatternWriter == null) {
+            if (negatedRelationPatternWriter == null) {
                 String negatedPatternFileName = dataDirectory + "ldpNegRelations";
-                negatedPatternWriter = new FileWriter(negatedPatternFileName);
+                negatedRelationPatternWriter = new FileWriter(negatedPatternFileName);
+            }
+            if (eventPatternWriter == null) {
+                String eventPatternFileName = dataDirectory +"EventModel";
+                eventPatternWriter = new FileWriter(eventPatternFileName);
+            }
+            if (negatedEventPatternWriter == null) {
+                String negatedPatternFileName = dataDirectory + "NegEvents";
+                negatedEventPatternWriter = new FileWriter(negatedPatternFileName);
             }
             if (!RECORD_NOUNS_AS_ENAMEX) {
                 buildEDTtypeFile(commonNounWriter);
             }
             buildOnoma(properNounWriter);
-            buildRelationPatternFile(relationPatternWriter, negatedPatternWriter);
-	    commonNounWriter = null;
-	    properNounWriter = null;
-	    relationPatternWriter = null;
-	    negatedPatternWriter = null;
+            buildRelationPatternFile(relationPatternWriter, negatedRelationPatternWriter);
+            buildEventPatternFile(eventPatternWriter, negatedEventPatternWriter);
+            commonNounWriter = null;
+            properNounWriter = null;
+            relationPatternWriter = null;
+            negatedRelationPatternWriter = null;
         } catch (IOException e) {
             e.printStackTrace(System.err);
         }
@@ -154,28 +174,53 @@ public class JetEngineBuilder {
         buildRelationPatternFile(w, negw, relations);
     }
 
+    public static void buildEventPatternFile(Writer w, Writer negw) throws IOException {
+        buildEventPatternFileFromNames(w, negw, new ArrayList<String>(Ice.events.keySet()));
+    }
+
+    public static void buildEventPatternFileFromNames(Writer w, Writer negw, List<String> eventNames) throws IOException {
+        List<IceEvent> events = new ArrayList<IceEvent>();
+        for (String type : eventNames) {
+            IceEvent rs = Ice.events.get(type);
+            if (null != rs) {
+                events.add(rs);
+            }
+        }
+        buildEventPatternFile(w, negw, events);
+    }
+
+    /**
+     *  Write relation patterns in ICE export format (pattern = eventType).
+     *  
+     *  @param  w           PrintWriter to which all [positive] patterns are written
+     *  @param  negw        PrintWriter to which all negative patterns [if any] are written
+     *  @param  relations   list of relations
+     */
+
     public static void buildRelationPatternFile(Writer w, Writer negw, List<IceRelation> relations) throws IOException {
         PrintWriter pw = new PrintWriter(w);
         for (IceRelation rs : relations) {
-            List<String> paths = rs.getBarePaths();
+            IcePath[] paths = rs.getBarePaths();
             String type = rs.getName();
-	    String arg1type = rs.getArg1type();
-	    String arg2type = rs.getArg2type();
-            for (String path : paths) {
-                boolean inv = rs.invertedPath(path);
-                String pattern = arg1type + "--" + path + "--" + arg2type;
+	        String arg1type = rs.getArg1type();
+	        String arg2type = rs.getArg2type();
+            for (IcePath path : paths) {
+                String pathString = path.getPathString();
+                boolean inv = rs.invertedPath(pathString);
+                String pattern = arg1type + "--" + pathString + "--" + arg2type;
                 pw.println(pattern + " = " + type + (inv ? "-1" : ""));
             }
         }
         pw.close();
         pw = new PrintWriter(negw);
         for (IceRelation rs : relations) {
-            List<String> paths = rs.getNegPaths();
+            IcePath[] paths = rs.getNegPaths();
             if (paths != null) {
                 String type = rs.getName();
-                for (String path : paths) {
-                    boolean inv = rs.invertedPath(path);
-                    String pattern = path.replaceAll(" -- ", "--");
+                for (IcePath path : paths) {
+                    String pathString = path.getPathString();
+                    boolean inv = rs.invertedPath(pathString);
+                    String pattern = path.getPathString().replaceAll(" -- ", "--");
                     pw.println(pattern + " = " + type + (inv ? "-1" : ""));
                 }
             }
@@ -183,5 +228,35 @@ public class JetEngineBuilder {
         pw.close();
     }
 
+    /**
+     *  Write event patterns in ICE export format (pattern = eventType).
+     *  
+     *  @param  w       PrintWriter to which all [positive] patterns are written
+     *  @param  negw    PrintWriter to which all negative patterns [if any] are written
+     *  @param  events  list of events
+     */
+
+    public static void buildEventPatternFile(Writer w, Writer negw, List<IceEvent> events) throws IOException {
+        PrintWriter pw = new PrintWriter(w);
+        for (IceEvent rs : events) {
+            List<IceTree> trees = rs.getTrees();
+            String type = rs.getName();
+            for (IceTree it : trees) {
+                pw.println(it.core() + " = " + type);
+            }
+        }
+        pw.close();
+        pw = new PrintWriter(negw);
+        for (IceEvent rs : events) {
+            List<IceTree> trees = rs.getNegTrees();
+            if (trees != null) {
+                String type = rs.getName();
+                for (IceTree it : trees) {
+                    pw.println(it.core() + " = " + type);
+                }
+            }
+        }
+        pw.close();
+    }
 }
 
