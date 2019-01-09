19191617f3d506b22ac91b00fbc20867daea18bc
Ralph Grishman <rdg@Ralphs-MacBook-Pro.local>
2019-01-08 20:18:43 -0500

    Add comment and error checks to IceCellRenderer.

--- src/main/java/edu/nyu/jet/ice/uicomps/IceCellRenderer.java
+++ src/main/java/edu/nyu/jet/ice/uicomps/IceCellRenderer.java
@@ -1,11 +1,19 @@
 package edu.nyu.jet.ice.uicomps;
 
 import edu.nyu.jet.ice.events.IceTree;
+import edu.nyu.jet.ice.models.IcePath;
 
 import javax.swing.*;
 import java.awt.*;
 
-public class IceCellRenderer extends JLabel implements ListCellRenderer {;
+/**
+ *  This cell renderer is intended to support displays of Lists
+ *  where the list elements are IcePaths or IceTrees.  What is
+ *  displayed is the "repr" (English phrase) in either case and
+ *  the choice made by the user regarding that phrase.
+ */
+
+public class IceCellRenderer extends JLabel implements ListCellRenderer {
 
     public IceCellRenderer() {
 	setOpaque(true);
@@ -17,16 +25,31 @@ public class IceCellRenderer extends JLabel implements ListCellRenderer {;
 	    boolean isSelected,
 	    boolean cellHasFocus) {
 
-	IceTree t = (IceTree) value;
-	String repr = t.getRepr();
-	IceTree.IceTreeChoice choice = t.getChoice();
-	if (choice  == IceTree.IceTreeChoice.YES)
-	    repr += " / YES";
-	else if (choice == IceTree.IceTreeChoice.NO)
-	    repr += " / NO";
-	setText(repr);
+    if (value == null) {
+        System.out.println ("CellRenderer got null");
+    } else if (value instanceof IceTree) {
+        IceTree t = (IceTree) value;
+        String repr = t.getRepr();
+        if (repr == null) System.out.println ("CellRenderer getting trees with null repr");
+        IceTree.IceTreeChoice choice = t.getChoice();
+        if (choice  == IceTree.IceTreeChoice.YES)
+            repr += " / YES";
+        else if (choice == IceTree.IceTreeChoice.NO)
+            repr += " / NO";
+        setText(repr);
+    } else if (value instanceof IcePath) {
+        IcePath t = (IcePath) value;
+        String repr = t.getRepr();
+        if (repr == null) System.out.println ("CellRenderer getting paths with null repr");
+        IcePath.IcePathChoice choice = t.getChoice();
+        if (choice  == IcePath.IcePathChoice.YES)
+            repr += " / YES";
+        else if (choice == IcePath.IcePathChoice.NO)
+            repr += " / NO";
+        setText(repr);
+    } else System.out.println ("Cell renderer got " + value);
 
-	Color background;
+	Color background; 
 	Color foreground;
 
 	// check if this cell is selected
